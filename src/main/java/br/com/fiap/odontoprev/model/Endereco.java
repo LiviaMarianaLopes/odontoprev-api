3ad1d318606d9451f9a0cb035a51f3e87dfb07b4
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2024-09-24 23:14:13 -0300

    :tada: Commit inicial

---
+++ src/main/java/br/com/fiap/odontoprev/model/Endereco.java
@@ -0,0 +1,4 @@
+package br.com.fiap.odontoprev.model;
+
+public class Endereco {
+}
---
+++ src/test/java/br/com/fiap/odontoprev/OdontoprevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.odontoprev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontoprevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
