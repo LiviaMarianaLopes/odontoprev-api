059202cfa953e31badc8ce715a01100b12240f75
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2024-10-29 19:16:47 -0300

    :sparkles: feat: Mensagens de erro no AuthController

--- src/main/java/br/com/fiap/odontoprev/controller/AuthController.java
+++ src/main/java/br/com/fiap/odontoprev/controller/AuthController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,17 +29,25 @@ public class AuthController {
     private TokenService tokenService;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid AuthDTO authDTO) {
-        var usuarioSenha = new UsernamePasswordAuthenticationToken(authDTO.email(), authDTO.senha());
-        var auth = this.authenticationManager.authenticate(usuarioSenha);
-        var token = tokenService.generateToken((Login) auth.getPrincipal());
-        return ResponseEntity.ok(new LoginResponse(token));
+    public ResponseEntity<?> login(@RequestBody @Valid AuthDTO authDTO) {
+        try {
+            var usuarioSenha = new UsernamePasswordAuthenticationToken(authDTO.email(), authDTO.senha());
+            var auth = this.authenticationManager.authenticate(usuarioSenha);
+            var token = tokenService.generateToken((Login) auth.getPrincipal());
+            return ResponseEntity.ok(new LoginResponse(token));
+        } catch (BadCredentialsException e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Credenciais inválidas");
+        }
     }
 
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid RegisterDTO registerDTO) {
         if (loginRepository.findByEmail(registerDTO.email()) != null) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity
+                    .badRequest()
+                    .body("O email já foi cadastrado");
         }
         String encryptedPassword = new BCryptPasswordEncoder().encode(registerDTO.senha());
         Login novoUsuario = new Login(registerDTO.email(), encryptedPassword);
